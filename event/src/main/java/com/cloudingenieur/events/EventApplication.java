792bd727d1e78dec94007b1bf86dde1b19b98df8
Mohammed <jamalmohamad.ik@gmail.com>
2024-07-20 20:26:58 +0200

    working keycloak, spring gateway and events microservices

--- event/src/main/java/com/cloudingenieur/events/EventApplication.java
+++ event/src/main/java/com/cloudingenieur/events/EventApplication.java
@@ -2,12 +2,21 @@ package com.cloudingenieur.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @SpringBootApplication
 public class EventApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventApplication.class, args);
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+		return new ResponseEntity<>("Unauthorized: " + ex.getMessage(), HttpStatus.UNAUTHORIZED);
 	}
 
 }
--- gateway/src/main/java/com/cloudingenieur/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cloudingenieur/gateway/GatewayApplication.java
@@ -1,5 +1,8 @@
 package com.cloudingenieur.gateway;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,21 +10,43 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
+
+
 
 @SpringBootApplication
-@EnableConfigurationProperties(UriConfiguration.class)
+//@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
 public class GatewayApplication {
 
-	@Bean
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
+
+	@GetMapping(value = "/token")
+	public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+	}
+
+	@GetMapping("/")
+	public Mono<String> index(WebSession session) {
+		return Mono.just(session.getId());
+	}
+
+	/*@Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
 		String httpUri = uriConfiguration.getHttpbin();
 		return builder.routes()
             .route(r -> r
-					.path("/api/v1/events")
+					.path("/callme/ping")
 					.filters(f -> f.addRequestHeader("Hello", "World"))
 					.uri(httpUri))
             .build();
-    }
+    } */
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -29,7 +54,7 @@ public class GatewayApplication {
 }
 
 
-@ConfigurationProperties
+/*@ConfigurationProperties
 class UriConfiguration {
 
 	// private String httpbin = "https://eventsservice.victoriousgrass-90628d5f.australiaeast.azurecontainerapps.io";
@@ -41,4 +66,4 @@ class UriConfiguration {
 	public void setHttpbin(String httpbin) {
 		this.httpbin = httpbin;
 	}
-}
+}*/
