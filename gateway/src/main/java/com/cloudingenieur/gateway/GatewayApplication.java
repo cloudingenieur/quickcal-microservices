ba8199eed6682fc088895e84e1bd6c3b755d0f7a
Mohammed <jamalmohamad.ik@gmail.com>
2024-07-24 00:07:22 +0200

    fix: add mysql database to POM.xml, setup properties file to use mysql.

--- gateway/src/main/java/com/cloudingenieur/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cloudingenieur/gateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package com.cloudingenieur.gateway;
 
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,28 +12,39 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
-
-
 
 @SpringBootApplication
 //@EnableConfigurationProperties(UriConfiguration.class)
 @RestController
 public class GatewayApplication {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
 
 	@GetMapping(value = "/token")
 	public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		System.out.println("Authorized client: " + authorizedClient.getAccessToken().getScopes());
 		return Mono.just(authorizedClient.getAccessToken().getTokenValue());
 	}
 
+
+	@GetMapping("/users")
+	public String users() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
+
+	}
 	@GetMapping("/")
 	public Mono<String> index(WebSession session) {
 		return Mono.just(session.getId());
