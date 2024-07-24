b64e7707cbb01c51e7c8d1a37a8222db60c4928e
Mohammed <jamalmohamad.ik@gmail.com>
2024-07-24 19:28:01 +0200

    fix: add getAllcustomers endpoint, implement paginations in bookings

--- booking/src/main/java/com/cloudingenieur/booking/controllers/CustomerController.java
+++ booking/src/main/java/com/cloudingenieur/booking/controllers/CustomerController.java
@@ -1,22 +1,56 @@
 package com.cloudingenieur.booking.controllers;
 
+import com.cloudingenieur.booking.entities.Customer;
+import com.cloudingenieur.booking.repositories.CustomerRepository;
+import com.cloudingenieur.booking.services.CustomerServices;
+import com.cloudingenieur.booking.util.JsonConverter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import jakarta.persistence.Tuple;
+import java.util.List;
+
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/bookings/v1")
 public class CustomerController {
 
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerServices customerServices;
+
+    @Autowired
+    private JsonConverter jsonConverter;
+
     @PreAuthorize("hasAuthority('SCOPE_TEST')")
-    @GetMapping("/customers")
-    public String getAllCustomers() {
+    @GetMapping("/scopes")
+    public String getScopes() {
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication authentication = context.getAuthentication();
         return "Scopes: " + authentication.getAuthorities();
+    }
+
+
+
+    @PreAuthorize("hasAuthority('SCOPE_TEST')")
+    @GetMapping("/customers")
+    public ResponseEntity<?> getAllCustomers(@RequestParam(defaultValue = "0") int page,
+                                             @RequestParam(defaultValue = "10") int size) {
+
+        var x = customerServices.getCustomers(0, 1);
+        System.out.println(x);
+
+        return ResponseEntity.status(HttpStatus.OK).body(customerServices.getCustomers(page, size));
+
     }
 }
---
+++ booking/src/main/java/com/cloudingenieur/booking/entities/Customer.java
@@ -0,0 +1,39 @@
+package com.cloudingenieur.booking.entities;
+
+import jakarta.persistence.*;
+import lombok.CustomLog;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "customer")
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "keycloakusername")
+    private String keycloakusername;
+
+    @Column(name = "keycloakemail")
+    private String keycloakemail;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "createdat")
+    private Date created_date;
+
+}
