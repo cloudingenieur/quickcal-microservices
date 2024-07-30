ba9a94fffe096e744d45eeb5468d869ce4f92db5
Mohammed <jamalmohamad.ik@gmail.com>
2024-07-31 00:36:11 +0200

    feat: integrate vue with spring boot and make maven profiles.

---
+++ frontend-driver/src/main/java/com/cloudingenieur/driver/StartFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.cloudingenieur.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartFrontendApplication.class, args);
+	}
+
+}
